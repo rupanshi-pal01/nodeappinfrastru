6bcc14dce48ac570ac6bfe29ca24d7ef51ba8505
rupanshi-pal01 <rupanshi.kx@gmail.com>
2021-03-22 11:58:52 +0530

    firstcommit

---
+++ main.tf
@@ -0,0 +1,81 @@
+resource "aws_launch_configuration" "agent-lc" {
+    name_prefix = "agent-lc-"
+    image_id = "ami-0c995ececfd9cdafe"
+    instance_type = "t2.micro"
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = "10"
+    }
+}
+resource "aws_autoscaling_group" "agents" {
+    availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+    name = "agents"
+    max_size = "20"
+    min_size = "1"
+    health_check_grace_period = 300
+    health_check_type = "EC2"
+    desired_capacity = 2
+    force_delete = true
+    launch_configuration = "${aws_launch_configuration.agent-lc.name}"
+
+    tag {
+        key = "Name"
+        value = "Agent Instance"
+        propagate_at_launch = true
+    }
+}
+resource "aws_autoscaling_policy" "agents-scale-up" {
+    name = "agents-scale-up"
+    scaling_adjustment = 1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+}
+
+resource "aws_autoscaling_policy" "agents-scale-down" {
+    name = "agents-scale-down"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+}
+resource "aws_cloudwatch_metric_alarm" "memory-high" {
+    alarm_name = "mem-util-high-agents"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "MemoryUtilization"
+    namespace = "System/Linux"
+    period = "300"
+    statistic = "Average"
+    threshold = "80"
+    alarm_description = "This metric monitors ec2 memory for high utilization on agent hosts"
+    alarm_actions = [
+        "${aws_autoscaling_policy.agents-scale-up.arn}"
+    ]
+    dimensions = {
+        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory-low" {
+    alarm_name = "mem-util-low-agents"
+    comparison_operator = "LessThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "MemoryUtilization"
+    namespace = "System/Linux"
+    period = "300"
+    statistic = "Average"
+    threshold = "40"
+    alarm_description = "This metric monitors ec2 memory for low utilization on agent hosts"
+    alarm_actions = [
+        "${aws_autoscaling_policy.agents-scale-down.arn}"
+    ]
+    dimensions = {
+        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+    }
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    access_key = "AKIARJYCDZM44PFJVCF5"
+    secret_key = "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"
+    region = "us-west-2"
+}
